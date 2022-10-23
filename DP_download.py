c4eafa35d0a6428d62cdbc4a7180e5df81c21543
shubham-pokhrel <115483662+shubham-pokhrel@users.noreply.github.com>
2022-10-23 19:56:48 +0545

    Add files via upload

---
+++ DP_download.py
@@ -0,0 +1,5 @@
+import instaloader
+il = instaloader.Instaloader()
+username = input("Enter username: ")
+il.download_profile(username, profile_pic_only = True)
+print(f"{username}'s DP is downloaded!!! ")
